00cab56e91d320ca3d7559fcbd27c7b4f7d873a9
ddundun <sincos0823@gmail.com>
2021-11-22 15:39:04 +0900

    211122

--- py_work/pyautoGUI/20211122/ex04.py
+++ py_work/pyautoGUI/20211122/ex04.py
@@ -1,9 +1,11 @@
+import time
 import pyautogui
 
-aa= pyautogui.locateOnScreen('aa.png')
+aa= pyautogui.locateOnScreen('aa.png', confidence = 0.8)
 pyautogui.click(aa)
 print(aa)
 
+time.sleep(2)
 bb=pyautogui.locateOnScreen('bb.png')
 pyautogui.click(bb)
 print(bb)
