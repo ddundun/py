29e066ab5f652a13408561aaf14eabdafc07fef1
ddundun <sincos0823@gmail.com>
2021-11-22 15:40:04 +0900

    211122

--- py_work/pyautoGUI/20211122/ex04.py
+++ py_work/pyautoGUI/20211122/ex04.py
@@ -6,6 +6,11 @@ pyautogui.click(aa)
 print(aa)
 
 time.sleep(2)
-bb=pyautogui.locateOnScreen('bb.png')
+bb=pyautogui.locateOnScreen('bb.png', confidence = 0.8)
 pyautogui.click(bb)
 print(bb)
+
+time.sleep(2)
+cc=pyautogui.locateOnScreen('cc.png', confidence = 0.8)
+pyautogui.click(cc)
+print(cc)
